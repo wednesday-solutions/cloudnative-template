942bc5f4db8e874f2c16ae2cb0e8229744f88df3
frostzt <aidenfrostbite@gmail.com>
2023-01-28 20:50:37 +0530

    chore: removed 90% coverage check till stable release

--- jest.config.js
+++ jest.config.js
@@ -11,13 +11,6 @@ module.exports = {
     "<rootDir>/test/",
   ],
   collectCoverage: true,
-  coverageThreshold: {
-    global: {
-      statements: 90,
-      branches: 90,
-      functions: 90,
-      lines: 90,
-    },
-  },
+  coverageThreshold: {},
   collectCoverageFrom: ["src/**/*.ts", "src/**/*.js"],
 };

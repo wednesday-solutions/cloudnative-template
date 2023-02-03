d3582f269de3d5fc7ce890d09b8e4363e6168562
Christin Itty John <christin@wednesday.is>
2023-02-04 00:29:34 +0530

    feat: add port and host args in health-check, use start:prod
    Signed-off-by: Christin Itty John <christin@wednesday.is>

--- dev/health-check.js
+++ dev/health-check.js
@@ -1,20 +1,24 @@
 const http = require('node:http');
+const { argv } = require('node:process');
+
+const port = argv[2];
+const host = argv[3];
 
 const options = {
   timeout: 2000,
-  host: 'localhost',
-  port: 5000,
-  path: '/health-check', // must be the same as HEALTHCHECK in Dockerfile
+  host,
+  port,
+  path: '/health-check',
 };
 
-const request = http.request(options, res => {
+const request = http.request(options, (res) => {
   console.info(`STATUS: ${res.statusCode}`);
   process.exitCode = res.statusCode === 200 ? 0 : 1;
   // eslint-disable-next-line unicorn/no-process-exit
   process.exit();
 });
 
-request.on('error', err => {
+request.on('error', (err) => {
   console.error('ERROR', err);
   // eslint-disable-next-line unicorn/no-process-exit
   process.exit(1);
